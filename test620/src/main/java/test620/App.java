c123fb71da0aa282c219afc1161027e08845af4d
kexiaomeng <307621564@qq.com>
2018-06-20 15:56:57 +0800

    620

---
+++ test620/src/main/java/test620/App.java
@@ -0,0 +1,13 @@
+package test620;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
